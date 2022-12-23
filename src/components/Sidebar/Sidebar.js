9c7677530dd0c651ce7e5fd5de2cabfe759e07c5
tamruthavarshini <t.varsha0529@gmail.com>
2022-12-23 16:46:14 +0530

    sidebar is completed

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,28 +1,29 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from 'react';
 import "./Sidebar.scss";
-import { Link } from "react-router-dom";
-import { useSelector,useDispatch } from "react-redux";
-import { getSidebarStatus,setSidebarOff } from "../../store/slidebarSlice";
-import { fetchAsyncCategories, getAllCategories } from "../../store/categorySlice";
+import {Link} from 'react-router-dom';
+import {useSelector, useDispatch} from 'react-redux';
+import { getSidebarStatus, setSidebarOff } from '../../store/slidebarSlice';
+import { fetchAsyncCategories, getAllCategories } from '../../store/categorySlice';
 
-const Sidebar = () =>
-{
-    const dispatch = useDispatch();
-    const isSidebarOn = useSelector(getSidebarStatus);
-    const categories  = useSelector(getAllCategories);
+const Sidebar = () => {
 
-    useEffect(() =>{
-        dispatch(fetchAsyncCategories())
-    },[dispatch])
-    return(
-        <aside className={`sidebar ${isSidebarOn ? 'hide-sidebar' : ""}`}>
+  const dispatch = useDispatch();
+  const isSidebarOn = useSelector(getSidebarStatus);
+  const categories = useSelector(getAllCategories);
+
+  useEffect(() => {
+    dispatch(fetchAsyncCategories())
+  }, [dispatch])
+
+  return (
+    <aside className={`sidebar ${isSidebarOn ? 'hide-sidebar' : ""}`}>
       <button type = "button" className='sidebar-hide-btn' onClick={() => dispatch(setSidebarOff())}>
         <i className='fas fa-times'></i>
       </button>
       <div className='sidebar-cnt'>
         <div className='cat-title fs-17 text-uppercase fw-6 ls-1h'>All Categories</div>
         <ul className='cat-list'>
-        {
+          {
             categories.map((category, idx) => {
               return (
                 <li key = {idx} onClick = {() => dispatch(setSidebarOff())}>
@@ -34,6 +35,7 @@ const Sidebar = () =>
         </ul>
       </div>
     </aside>
-    );
+  )
 }
+
 export default Sidebar;
