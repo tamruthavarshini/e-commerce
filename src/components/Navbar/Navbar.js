b23e1ac11ec16d6cbb80f8b47a191ea122bec346
tamruthavarshini <t.varsha0529@gmail.com>
2022-12-23 16:49:33 +0530

    navbar is completed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,14 +5,20 @@ import { useSelector, useDispatch} from 'react-redux';
 import { setSidebarOn } from '../../store/slidebarSlice';
 import { getAllCategories } from '../../store/categorySlice';
 import { getAllCarts, getCartItemsCount, getCartTotal } from '../../store/cartSlice';
-import CartModal from '../CartModal/CartModal';
+import CartModal from "../CartModal/CartModal";
 
 const Navbar = () => {
   const dispatch = useDispatch();
   const categories = useSelector(getAllCategories);
   const carts = useSelector(getAllCarts);
   const itemsCount = useSelector(getCartItemsCount);
-  
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchTerm = (e) => {
+    e.preventDefault();
+    setSearchTerm(e.target.value);
+  }
+
   useEffect(() => {
     dispatch(getCartTotal());
   }, [carts])
@@ -37,8 +43,8 @@ const Navbar = () => {
         <div className='navbar-collapse w-100'>
           <div className='navbar-search bg-white'>
             <div className='flex align-center'>
-              <input type = "text" className='form-control fs-14' placeholder='Search your preferred items here' />
-              <Link to = "" className='text-white search-btn flex align-center justify-center'>
+              <input type = "text" className='form-control fs-14' placeholder='Search your preferred items here' onChange={(e) => handleSearchTerm(e)} />
+              <Link to = {`search/${searchTerm}`} className='text-white search-btn flex align-center justify-center'>
                   <i className='fa-solid fa-magnifying-glass'></i>
                 </Link>
             </div>
